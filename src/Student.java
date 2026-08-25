b9baebdc457e3c9a0b511515309b623c5fbc7b1b
snehaa-16 <mandalsneha33@gmail.com>
2026-08-25 12:13:40 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,62 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+    private double marks;
+
+    public Student(int id, String name, int age, String course, double marks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    public void displayStudent() {
+        System.out.println("----------------------------");
+        System.out.println("Student ID : " + id);
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Course     : " + course);
+        System.out.println("Marks      : " + marks);
+        System.out.println("----------------------------");
+    }
+}
